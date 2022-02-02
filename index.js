7dfb94e832894930fa3784f853d76a2102ae7026
Spencer Johnson <sljohnson2222@gmail.com>
2022-02-02 16:28:14 -0600

    commit tree 28678f35417516510b89d8d4199d559902b90202

--- index.js
+++ index.js
@@ -1,40 +1,7 @@
-const cats = [ "Milo", "Otis", "Garfield"];
-
 beforeEach(function () {
     cats.length = 0;
   
     cats.push("Milo", "Otis", "Garfield");
-  }
-);
+  });
 
-function destructivelyAppendCat() {
-    cats.push("Ralph");
-}
-
-function destructivelyPrependCat() {
-    cats.unshift("Bob");
-}
-
-function destructivelyRemoveLastCat() {
-    cats.pop();
-}
-
-function destructivelyRemoveFirstCat() {
-    cats.shift();
-}
-
-function appendCat(copyOfCats) {
-    return copyOfCats = [...cats,"Broom"]
-}
-
-function prependCat(copyOfCats) {
-    return copyOfCats = ["Arnold", ...cats];
-}
-
-function removeLastCat() {
-    return cats.slice(0,-1);
-}
-
-function removeFirstCat() {
-    return cats.slice(1);
-}
+const cats = ["Milo", "Otis", "Garfield"];
